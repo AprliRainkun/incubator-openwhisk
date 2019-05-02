b115ae19e4c845c855551391a1c3d7bfeadf72f6
Mingyu Zhou <zmy.2347@sjtu.edu.cn>
2019-05-02 11:25:59 +0800

    temp save

--- core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/Queue.scala
+++ core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/Queue.scala
@@ -1,31 +1,39 @@
 package org.apache.openwhisk.core.scheduler
 
-import akka.actor.{Actor, ActorRef, Props, Terminated}
-import akka.stream.scaladsl.Flow
+import akka.actor.{Actor, ActorRef, Props, Timers}
 import akka.stream.{ActorMaterializer, Materializer}
-import akka.util.Timeout
 
 import scala.collection.immutable
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 object Queue {
   def props(name: String) = Props(new Queue(name))
 
-  final case object RegisterStage
-  final case object FetchRequest
-  final case class Fetched(activation: DummyActivation)
+  final case class Handle(queue: ActorRef, key: Long)
+  final case class CancelFetch(key: Long)
+  final case class RequestAtMost(key: Long, n: Int)
+  final case class Response(as: List[DummyActivation])
+
+  // fetcher will be set to Some(f) when the first RequestAtMost message is received
+  private case class Registration(n: Int, fetcher: Option[ActorRef])
+  private case object TKey
+  private case object TTick
 }
 
-class Queue(name: String) extends Actor {
+class Queue(name: String) extends Actor with Timers {
   import Queue._
   import QueueManager._
 
   implicit val ex: ExecutionContext = context.dispatcher
   implicit val mat: Materializer = ActorMaterializer()
 
+  private var counter = 0L
   private var queue = immutable.Queue.empty[DummyActivation]
-  private var fetchers = Map.empty[ActorRef, Int]
+  private var fetchers = Map.empty[Long, Registration]
+
+  timers.startPeriodicTimer(TKey, TTick, 5.seconds)
 
   override def receive: Receive = {
     case AppendActivation(act) =>
@@ -33,38 +41,58 @@ class Queue(name: String) extends Actor {
       sender ! Right(Unit)
       trySend(None)
     case EstablishFetchStream(windows) =>
-      implicit val timeout: Timeout = Timeout(5.seconds)
+      val key = nextKey
+      fetchers += (key -> Registration(0, None))
 
-      val flow = Flow.fromGraph(new ActivationStage(self))
+      val flow = ReactiveActivationFlow.create(Handle(self, key))
       val stream = windows.map(w => w.getWindowsSize).via(flow)
       sender ! Right(FetchStream(stream))
-    case RegisterStage =>
-      fetchers += (sender -> 0)
-      context.watch(sender)
-    case FetchRequest =>
-      val oldVal = fetchers(sender)
-      fetchers += (sender -> (oldVal + 1))
-      trySend(Some(sender))
-    case Terminated(f) =>
-      fetchers -= f
+    case RequestAtMost(key, n) =>
+      val old = fetchers(key).n
+      fetchers += (key -> Registration(old + n, Some(sender)))
+      trySend(Some(key))
+    case CancelFetch(key) =>
+      val Registration(n, Some(f)) = fetchers(key)
+      // send en empty response to allow the ask future to complete
+      if (n > 0) {
+        f ! Response(List.empty)
+      }
+      fetchers -= key
+    case TTick =>
+      // prevent ask future from timing out
+      fetchers collect {
+        case (_, Registration(n, Some(f))) if n > 0 => f
+      } foreach { f =>
+        f ! Response(List.empty)
+      }
   }
 
-  private def trySend(hint: Option[ActorRef]): Unit = {
-    hint map { f =>
-      (f, fetchers(f))
+  private def nextKey = {
+    counter += 1
+    counter
+  }
+
+  private def trySend(hint: Option[Long]): Unit = {
+    hint map { key =>
+      (key, fetchers(key).n)
     } orElse {
       fetchers collectFirst {
-        case (fetcher, window) if window > 0 => (fetcher, window)
+        case (key, Registration(n, _)) if n > 0 => (key, n)
       }
     } foreach {
-      case (fetcher, window) =>
-        val send = Math.min(window, queue.size)
-        (1 to send) foreach { _ =>
-          val (elem, newQueue) = queue.dequeue
-          fetcher ! Fetched(elem)
-          queue = newQueue
+      case (key, n) =>
+        val send = Math.min(n, queue.size)
+        if (send > 0) {
+          val es = ListBuffer.empty[DummyActivation]
+          (1 to send) foreach { _ =>
+            val (elem, newQueue) = queue.dequeue
+            es.append(elem)
+            queue = newQueue
+          }
+          val fO = fetchers(key).fetcher
+          fetchers += (key -> Registration(n - send, fO))
+          fO.head ! Response(es.toList)
         }
-        fetchers += (fetcher -> (window - send))
     }
   }
 }
---
+++ core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/ReactiveActivationFlow.scala
@@ -0,0 +1,111 @@
+package org.apache.openwhisk.core.scheduler
+
+import java.time._
+
+import akka.NotUsed
+import akka.pattern.ask
+import akka.stream._
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.util.Timeout
+
+object ReactiveActivationFlow {
+  def create(handle: Queue.Handle): Flow[Int, DummyActivation, NotUsed] = {
+    implicit val to: Timeout = Timeout.create(Duration.ofSeconds(10))
+
+    val fetchFlow: Flow[Int, List[DummyActivation], NotUsed] = Flow[Int].mapAsync(1) { n =>
+      (handle.queue ? Queue.RequestAtMost(handle.key, n)).mapTo[Queue.Response]
+    } map (_.as)
+
+    val flatFlow = Flow[List[DummyActivation]].mapConcat[DummyActivation](identity)
+
+    Flow.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val broadcast = b.add(Broadcast[List[DummyActivation]](2))
+      val repeater = b.add(new RepeatUntilAccumulateToStage(handle))
+      val fetcher = b.add(fetchFlow)
+      val flatter = b.add(flatFlow)
+
+      // format: off
+      repeater.out ~>                 fetcher                 ~> broadcast ~> flatter
+      repeater.in1 <~ Flow[List[DummyActivation]].map(_.size) <~ broadcast
+      // format: on
+      FlowShape(repeater.in0, flatter.out)
+    })
+  }
+}
+
+class RepeatUntilAccumulateToStage(handle: Queue.Handle) extends GraphStage[FanInShape2[Int, Int, Int]] {
+  // nonzero number
+  val inTarget: Inlet[Int] = Inlet("target-source-in")
+  // could be zero
+  val inFeedback: Inlet[Int] = Inlet("feedback-in")
+  // nonzero number
+  val out: Outlet[Int] = Outlet("round-out")
+
+  override def shape: FanInShape2[Int, Int, Int] = new FanInShape2(inTarget, inFeedback, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+    var remaining = 0
+    var pendingFeedback = false
+
+    override def preStart(): Unit = {
+      pull(inTarget)
+      pull(inFeedback)
+    }
+
+    setHandler(
+      inTarget,
+      new InHandler {
+        override def onPush(): Unit = {
+          remaining = grab(inTarget)
+          if (isAvailable(out)) {
+            sendRemaining()
+          }
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          handle.queue ! Queue.CancelFetch(handle.key)
+          if (!pendingFeedback) {
+            completeStage()
+          }
+        }
+      })
+
+    setHandler(
+      inFeedback,
+      new InHandler {
+        override def onPush(): Unit = {
+          remaining -= grab(inFeedback)
+          pendingFeedback = false
+
+          if (isClosed(inTarget)) {
+            completeStage()
+            return
+          }
+
+          pull(inFeedback)
+          if (remaining == 0) {
+            pull(inTarget)
+          }
+          if (isAvailable(out)) {
+            sendRemaining()
+          }
+        }
+      })
+
+    setHandler(out, new OutHandler {
+      override def onPull(): Unit = {
+        if (remaining > 0 && !pendingFeedback) {
+          sendRemaining()
+        }
+      }
+    })
+
+    private def sendRemaining(): Unit = {
+      push(out, remaining)
+      pendingFeedback = true
+    }
+  }
+}
