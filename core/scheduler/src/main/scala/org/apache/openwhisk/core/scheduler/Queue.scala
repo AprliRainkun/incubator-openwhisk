a09144d36560dbc81fe0cefc8ec32a0cfccf1055
Mingyu Zhou <zmy.2347@sjtu.edu.cn>
2019-05-02 16:26:02 +0800

    migrate complete graph stage to graph dsl

--- core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/Queue.scala
+++ core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/Queue.scala
@@ -1,70 +1,103 @@
 package org.apache.openwhisk.core.scheduler
 
-import akka.actor.{Actor, ActorRef, Props, Terminated}
-import akka.stream.scaladsl.Flow
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Timers}
 import akka.stream.{ActorMaterializer, Materializer}
-import akka.util.Timeout
 
 import scala.collection.immutable
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 object Queue {
   def props(name: String) = Props(new Queue(name))
 
-  final case object RegisterStage
-  final case object FetchRequest
-  final case class Fetched(activation: DummyActivation)
+  final case class Handle(queue: ActorRef, key: Long)
+  final case class CancelFetch(key: Long)
+  // n most be nonzero
+  final case class RequestAtMost(key: Long, n: Int)
+  final case class Response(as: List[DummyActivation])
+
+  // fetcher will be set to Some(f) when the first RequestAtMost message is received
+  private case class Registration(n: Int, fetcher: ActorRef)
+  private case object TKey
+  private case object TTick
 }
 
-class Queue(name: String) extends Actor {
+class Queue(name: String) extends Actor with Timers with ActorLogging {
   import Queue._
   import QueueManager._
 
   implicit val ex: ExecutionContext = context.dispatcher
   implicit val mat: Materializer = ActorMaterializer()
 
+  private var counter = 0L
   private var queue = immutable.Queue.empty[DummyActivation]
-  private var fetchers = Map.empty[ActorRef, Int]
+  private var waitingList = Map.empty[Long, Registration]
+
+  timers.startPeriodicTimer(TKey, TTick, 5.seconds)
 
   override def receive: Receive = {
     case AppendActivation(act) =>
       queue = queue.enqueue(act)
       sender ! Right(Unit)
-      trySend(None)
+      log.debug(s"append activation to queue, new len ${queue.size}")
+      servePendingFetch()
     case EstablishFetchStream(windows) =>
-      implicit val timeout: Timeout = Timeout(5.seconds)
+      val key = nextKey
 
-      val flow = Flow.fromGraph(new ActivationStage(self))
+      val flow = ReactiveActivationFlow.create(Handle(self, key))
       val stream = windows.map(w => w.getWindowsSize).via(flow)
       sender ! Right(FetchStream(stream))
-    case RegisterStage =>
-      fetchers += (sender -> 0)
-      context.watch(sender)
-    case FetchRequest =>
-      val oldVal = fetchers(sender)
-      fetchers += (sender -> (oldVal + 1))
-      trySend(Some(sender))
-    case Terminated(f) =>
-      fetchers -= f
+    case RequestAtMost(key, n) =>
+      log.debug(s"fetcher $key request $n")
+      if (queue.nonEmpty) {
+        dispatchAtMostNToFetcher(n, sender, key)
+      } else {
+        waitingList += (key -> Registration(n, sender))
+      }
+    case CancelFetch(key) =>
+      log.debug(s"received cancel from fetcher $key")
+      if (waitingList.contains(key)) {
+        val Registration(n, f) = waitingList(key)
+        // send en empty response to allow the ask future to complete
+        log.debug(
+          s"fetcher $key has requested $n elems previously, now send empty to allow the pending ask to complete")
+        f ! Response(List.empty)
+        waitingList -= key
+      }
+    case TTick =>
+      // prevent ask future from timing out
+      waitingList collect {
+        case (_, Registration(_, f)) => f
+      } foreach { f =>
+        f ! Response(List.empty)
+      }
   }
 
-  private def trySend(hint: Option[ActorRef]): Unit = {
-    hint map { f =>
-      (f, fetchers(f))
-    } orElse {
-      fetchers collectFirst {
-        case (fetcher, window) if window > 0 => (fetcher, window)
-      }
+  private def nextKey = {
+    counter += 1
+    counter
+  }
+
+  private def dispatchAtMostNToFetcher(n: Int, fetcher: ActorRef, key: Long): Unit = {
+    val send = Math.min(n, queue.size)
+    val es = ListBuffer.empty[DummyActivation]
+    (1 to send) foreach { _ =>
+      val (elem, newQueue) = queue.dequeue
+      es.append(elem)
+      queue = newQueue
+    }
+    log.debug(s"dispatch ${es.size} acts to fetcher $key")
+    fetcher ! Response(es.toList)
+  }
+
+  private def servePendingFetch(): Unit = {
+    waitingList collectFirst {
+      case (key, Registration(n, fetcher)) => (key, n, fetcher)
     } foreach {
-      case (fetcher, window) =>
-        val send = Math.min(window, queue.size)
-        (1 to send) foreach { _ =>
-          val (elem, newQueue) = queue.dequeue
-          fetcher ! Fetched(elem)
-          queue = newQueue
-        }
-        fetchers += (fetcher -> (window - send))
+      case (key, n, fetcher) =>
+        dispatchAtMostNToFetcher(n, fetcher, key)
+        waitingList -= key
     }
   }
 }
--- tests/src/test/scala/org/apache/openwhisk/core/scheduler/test/QueueServiceTests.scala
+++ tests/src/test/scala/org/apache/openwhisk/core/scheduler/test/QueueServiceTests.scala
@@ -1,5 +1,6 @@
 package org.apache.openwhisk.core.scheduler.test
 
+import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.testkit.scaladsl.TestSink
 import com.google.protobuf.ByteString
 import org.apache.openwhisk.grpc.WindowAdvertisement.Message
@@ -9,6 +10,7 @@ import org.scalatest.OptionValues._
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
 
 class QueueServiceTests extends TestBase("QueueCreationTests") with LocalScheduler {
 
@@ -77,6 +79,38 @@ class QueueServiceTests extends TestBase("QueueCreationTests") with LocalSchedul
       succeed
     }
 
+    "serve concurrent fetchers" in {
+      val actionName = "ns/pkg/act23234"
+      val putNumberUnit = 10
+      val fetcherNumber = 3
+
+      val createFut = createQueue(actionName) map { resp =>
+        resp.status.value.statusCode should be(200)
+      }
+      Await.result(createFut, 3.seconds)
+
+      val fetchAsserts = (1 to fetcherNumber) map { _ =>
+        val windows = (Message.ActionName(actionName) :: (1 to 4).map(Message.WindowsSize).toList) map { m =>
+          WindowAdvertisement(m)
+        }
+        schedulerClient
+          .fetch(Source(windows))
+          .map(_ => 1)
+          .runWith(Sink.fold(0)(_ + _))
+          .map(n => n should be(10))
+      } toList
+
+      val putFut = seedNPuts(actionName, putNumberUnit * fetcherNumber)
+
+      val combinedFut = for {
+        _ <- Future.traverse(fetchAsserts)(identity)
+        _ <- putFut
+      } yield ()
+      Await.result(combinedFut, 20.seconds)
+
+      succeed
+    }
+
     "not reply with any message when the queue is drained" in {
       val actionName = "ns/pkg/act333"
       val putNumber = 3
@@ -136,7 +170,7 @@ class QueueServiceTests extends TestBase("QueueCreationTests") with LocalSchedul
       sender.sendNext(WindowAdvertisement(Message.ActionName(actionName)))
       recv.request(10)
 
-      (1 to 4) foreach {n =>
+      (1 to 4) foreach { n =>
         sender.sendNext(WindowAdvertisement(Message.WindowsSize(n)))
         recv.expectNextN(n)
         // the window should be depleted, no activation should be received
@@ -145,6 +179,36 @@ class QueueServiceTests extends TestBase("QueueCreationTests") with LocalSchedul
 
       sender.sendComplete()
       recv.expectComplete()
+
+      succeed
+    }
+
+    "not loss activation when fetcher cancelled" in {
+      val actionName = "ns/pkg/act555"
+      val putNum = 10
+
+      val putFut = for {
+        resp <- createQueue(actionName)
+        _ = resp.status.value.statusCode should be(200)
+        _ <- seedNPuts(actionName, putNum)
+      } yield ()
+      Await.result(putFut, 3.seconds)
+
+      (1 to 4) foreach { n =>
+        val (sender, source) = Utils.feedAndSource[WindowAdvertisement]
+        val recv = schedulerClient.fetch(source).runWith(TestSink.probe)
+
+        List(Message.ActionName(actionName), Message.WindowsSize(n))
+          .map(WindowAdvertisement(_))
+          .foreach(sender.sendNext)
+
+        recv.request(2 * n)
+        recv.expectNextN(n)
+        recv.expectNoMessage(3.seconds)
+
+        sender.sendComplete()
+        recv.expectComplete()
+      }
 
       succeed
     }
